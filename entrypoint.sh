7c4cf1c27781625b46701b5acf59b7ee78dfa9c5
Mike Francis <mikeffrancis@gmail.com>
2021-10-21 14:00:50 +0100

    Use input arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
 IFS=/ read -a REPOSITORY_PARTS <<< "$GITHUB_REPOSITORY"
-DEPLOYMENT_URL=$(curl -H "Content-type: application/json" -H "Authorization: Bearer $VERCEL_TOKEN" "https://api.vercel.com/v5/deployments?meta-githubRepo=${REPOSITORY_PARTS[1]}&meta-githubCommitRef=$GITHUB_HEAD_REF" | jq -r '.deployments[0].url')
+DEPLOYMENT_URL=$(curl -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${REPOSITORY_PARTS[1]}&meta-githubCommitRef=$GITHUB_HEAD_REF" | jq -r '.deployments[0].url')
 npx cypress run --config baseUrl="https://$DEPLOYMENT_URL"
