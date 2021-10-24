7f8090ac33a1b1909ee27224c95deeab78ae02ee
Mike Francis <mikeffrancis@gmail.com>
2021-10-24 14:37:25 +0100

    Use different env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 echo "GITHUB_REPOSITORY: $GITHUB_REPOSITORY"
-echo "GITHUB_HEAD_REF: $GITHUB_HEAD_REF"
+echo "GITHUB_REF: $GITHUB_REF"
 echo "TOKEN: $1"
-DEPLOYMENT_URL=$(curl -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${GITHUB_REPOSITORY}&meta-githubCommitRef=$GITHUB_HEAD_REF" | jq -r '.deployments[0].url')
+DEPLOYMENT_URL=$(curl -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${GITHUB_REPOSITORY}" | jq -r '.deployments[0].url')
 npx cypress run --config baseUrl="https://$DEPLOYMENT_URL"
