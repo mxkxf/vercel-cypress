4f7b20c1d9048052b189eb0e8cc17dce44126ad2
Mike Francis <mikeffrancis@gmail.com>
2021-10-24 16:38:14 +0100

    Use repo arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -l
 
-echo "GITHUB_REPOSITORY: $GITHUB_REPOSITORY"
-echo "GITHUB_REF: $GITHUB_REF"
+echo "GITHUB_ACTION_REF: $GITHUB_ACTION_REF"
 echo "TOKEN: $1"
-DEPLOYMENT_URL=$(curl -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${GITHUB_REPOSITORY}" | jq -r '.deployments[0].url')
+echo "REPO: $2"
+echo "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}"
+DEPLOYMENT_URL=$(curl -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
 npx cypress run --config baseUrl="https://$DEPLOYMENT_URL"
