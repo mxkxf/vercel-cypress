10aaa3892d44f10cdce9389bea0616d49259b2c4
Mike Francis <mikeffrancis@gmail.com>
2021-10-24 17:34:30 +0100

    Install npm deps in action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -l
 
-echo "GITHUB_ACTION_REF: $GITHUB_ACTION_REF"
-echo "TOKEN: $1"
-echo "REPO: $2"
-echo "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}"
 DEPLOYMENT_URL=$(curl -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
+npm ci
 npx cypress run --config baseUrl="https://$DEPLOYMENT_URL"
