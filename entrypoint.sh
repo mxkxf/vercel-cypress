dcc309c5dd02dd325a7ca275f4b20277a0245f41
Mike Francis <mikeffrancis@gmail.com>
2021-11-01 21:14:23 +0000

    More FFS

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 DEPLOYMENT_URL=$(curl -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
 npm install
-cypress run --config baseUrl="https://$DEPLOYMENT_URL"
+npx cypress run --config baseUrl="https://$DEPLOYMENT_URL"
