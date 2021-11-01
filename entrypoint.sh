80849a891906f51846eeeee5ed54c95e1e8ab6d5
Mike Francis <mikeffrancis@gmail.com>
2021-11-01 21:22:27 +0000

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh -l
 
+echo "TOKEN" $1
+echo "REPO" $2
+
 DEPLOYMENT_URL=$(curl -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
 npm install
 npx cypress run --config baseUrl="$DEPLOYMENT_URL"
