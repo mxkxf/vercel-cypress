c5665fedb94686a092b35ca0f117a99bee7b4320
Mike Francis <mikeffrancis@gmail.com>
2021-11-01 21:37:33 +0000

    Add prefix back in

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -l
 
-curl --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}"
-
 DEPLOYMENT_URL=$(curl --insecure -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
 npm install
-npx cypress run --config baseUrl="$DEPLOYMENT_URL"
+npx cypress run --config baseUrl="https://$DEPLOYMENT_URL"
