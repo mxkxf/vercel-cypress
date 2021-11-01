62e2ba43b31268ec8c4d0ccc9b5f93f5b312bc80
Mike Francis <mikeffrancis@gmail.com>
2021-11-01 20:32:59 +0000

    Info about cypress-axe

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 
 DEPLOYMENT_URL=$(curl -H "Content-type: application/json" -H "Authorization: Bearer $1" "https://api.vercel.com/v5/deployments?meta-githubRepo=${2}" | jq -r '.deployments[0].url')
 npm ci
+npm info cypress-axe
 npx cypress run --config baseUrl="https://$DEPLOYMENT_URL"
